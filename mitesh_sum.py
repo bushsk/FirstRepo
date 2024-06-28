780c3720d0e135e6394839255f77b4b52392a18d
MITESHDNAIK <mitesh785naik@gmail.com>
2024-06-28 12:20:24 +0530

    I am pushing, just go

---
+++ mitesh_sum.py
@@ -0,0 +1,9 @@
+#name 
+print("Hello, This is Mitesh Naik")
+
+#sum
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+total = number1 + number2
+
+print("The sum of the two numbers is: " + str(total))
