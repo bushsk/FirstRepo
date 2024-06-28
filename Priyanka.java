667b9f4c6cc87c5d237b808d854ca7ec7aecae0b
Priyanka Gounder <127594944+Priyanka03012005@users.noreply.github.com>
2024-06-28 12:34:23 +0530

    Update Priyanka.java

--- Priyanka.java
+++ Priyanka.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 class Priyanka {
     public static void main(String[] args) {
-        System.out.println("Enter the number to find Factorial:");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 5;
         int fact=1;
         int i;
         for(i=1;i<=n;i++){
